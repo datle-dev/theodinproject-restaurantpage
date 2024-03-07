d960e3dac04fca58b3d82aa5e05af2fd6d53112f
datle-dev <dle.dev@outlook.com>
2024-03-07 15:01:55 -0500

    Add IIFE to handle tab switching

--- src/index.js
+++ src/index.js
@@ -1,9 +1,63 @@
 import './style.css';
-import initPage from './init-page';
+import createHomePage from './create-home-page';
+import createAboutPage from './create-about-page';
+import createMenuPage from './create-menu-page';
+
+(function TabDisplay() {
+
+    const homeTab = createHomePage();
+    const aboutTab = createAboutPage();
+    const menuTab = createMenuPage();
+
+    const homeTabBtn = document.querySelector('#home-tab')
+    const aboutTabBtn = document.querySelector('#about-tab')
+    const menuTabBtn = document.querySelector('#menu-tab')
+
+    const content = document.querySelector('#content');
+    console.log(homeTabBtn);
+    console.log(content);
+    console.log(homeTab);
+
+    const assignEventListeners = () => {
+        homeTabBtn.addEventListener('click', showHomePage);
+        aboutTabBtn.addEventListener('click', showAboutPage);
+        menuTabBtn.addEventListener('click', showMenuPage);
+    };
+
+    const init = () => {
+        content.appendChild(homeTab);
+        assignEventListeners();
+    };
 
 
-const homePage = initPage();
+    const clearContents = () => {
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+    };
 
-const content = document.querySelector('#content');
+    const showHomePage = () => {
+        clearContents();
+        content.appendChild(homeTab);
+    };
 
-content.appendChild(homePage);
+    const showAboutPage = () => {
+        clearContents();
+        content.appendChild(aboutTab);
+    };
+
+    const showMenuPage = () => {
+        clearContents();
+        content.appendChild(menuTab);
+    };
+
+    // initiailziation on page load
+    init();
+
+})();
+
+
+
+
+
+
