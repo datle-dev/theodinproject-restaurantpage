662edd8fe13b8e66c50e1eb9a79dcff567735455
datle-dev <dle.dev@outlook.com>
2024-03-07 13:55:20 -0500

    Add code to import and append image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
+import HeroImg from './assets/cafe.jpg';
 
 console.log('hello world');
+
+const content = document.querySelector('#content');
+const heroDiv = document.createElement('div');
+
+const heroImg = new Image();
+
+heroDiv.id = 'hero';
+
+heroImg.src = HeroImg;
+heroImg.alt = 'cafe with seating';
+
+heroDiv.appendChild(heroImg);
+content.appendChild(heroDiv);
+
